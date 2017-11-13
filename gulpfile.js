8b707b484679e1c023b327f0ba0984f90fa5aea1
e200 <eleandro@inbox.ru>
2017-11-13 01:18:43 +0100

    Added gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+const
+    // Requires gulp
+    gulp = require('gulp')
+
+    // Requires browser-sync
+    browserSync = require('browser-sync')
+
+    //Requires gulp-uglify
+    uglify = require('gulp-uglify')
+
+    // Requires pumbler
+    pumbler = require('gulp-plumber')
+
+const
+    projectFolder = './'
+    srcFolder = projectFolder + 'src/'
+    distFolder = projectFolder + 'dist/'
+
+gulp.task('js:minify', function(){
+    const uglifyOptions = {
+        compress: {
+            drop_debugger: true,
+            toplevel: true
+        },
+        mangle: {
+            toplevel: true
+        }
+    }
+
+    return gulp.src([srcFolder + 'slim.js'])
+        .pipe(pumbler())
+        .pipe(uglify(uglifyOptions))
+        .pipe(gulp.dest(distFolder))
+})
+
+gulp.task('js:watch', function(){
+    browserSync.init({
+        server: './'
+    })
+
+    gulp.watch(['./src/**', './tests/**'])
+        .on('change', browserSync.reload)
+})
+
+gulp.task('default', ['js:watch'])
